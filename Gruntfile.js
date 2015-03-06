450bfa918d9297bb369c1d8ead910ad75661eee6
Romain Fleury <rfleury@digitaleo.com>
2015-03-06 14:08:04 +0100

    ADD Gruntfile, package.json, eslint build

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+		eslint: {
+			all: ['bestPlayerApp/**/*.js']
+		}
+    });
+	
+	grunt.loadNpmTasks('eslint-grunt');
+	
+	grunt.registerTask('default',[
+		'eslint'
+	]);
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # players-ratings
 Web application build to store games results, calculate players rank, create championships...
+
+
+# Use
+
+Simply install http-server npm package :
+    sudo npm install -g http-server
+
+Then serve public folder :
+
+	http-server bestPlayerApp/public/
+
+# Code checks
+
+Grunt default checks all js files.
+
+	grunt
